a00956c12abfea09fbad92da387221b08f7d37f0
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-04-04 23:39:57 -0300

    Strapi populate URL

--- blog-next/pages/index.js
+++ blog-next/pages/index.js
@@ -3,9 +3,10 @@ export default function Home(props) {
     <div>
       <div>
         {props.posts.map((post) => (
-          <h2 key={post.id}>
-            {post.attributes.Title}
-          </h2>
+          <div key={post.id}>
+            <h2>{post.attributes.Title}</h2>
+            <p>{post.attributes.User.data.attributes.username}</p>
+          </div>
         ))}
       </div>
     </div>
@@ -14,7 +15,7 @@ export default function Home(props) {
 
 export async function getStaticProps(){
   // get posts from our api
-  const res = await fetch('http://localhost:1337/api/posts');
+  const res = await fetch('http://localhost:1337/api/posts?populate=%2A');
   const data = await res.json();
   const posts = data.data;
 
